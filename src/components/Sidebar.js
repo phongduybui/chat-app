152aefa5813f9cff1b42d793da0e5f46a408047c
phongduybui <buiduyphong921@gmail.com>
2021-07-12 21:51:20 +0700

    Fix sidebar ui

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import { ReactComponent as Logo } from '../assets/icons/logo.svg';
 import {
   BiChat,
@@ -9,51 +9,53 @@ import {
   BiMoon,
   BiCog,
 } from 'react-icons/bi';
+import Avatar from './Avatar';
 
 const Sidebar = () => {
   return (
     <aside className='Sidebar'>
       <div className='brand'>
-        <Logo width={22} height={22} />
+        <NavLink to='/'>
+          <Logo width={22} height={22} />
+        </NavLink>
       </div>
       <ul className='nav'>
         <li className='nav__item'>
-          <Link to='/chats' className='nav__link'>
+          <NavLink exact to='/' className='nav__link'>
             <BiChat />
-          </Link>
+          </NavLink>
         </li>
         <li className='nav__item sm-hide'>
-          <Link to='/groups' className='nav__link'>
+          <NavLink to='/groups' className='nav__link'>
             <BiGroup />
-          </Link>
+          </NavLink>
         </li>
         <li className='nav__item'>
-          <Link to='/calls' className='nav__link'>
+          <NavLink to='/calls' className='nav__link'>
             <BiPhone />
-          </Link>
+          </NavLink>
         </li>
         <li className='nav__item'>
-          <Link to='/contacts' className='nav__link'>
+          <NavLink to='/contacts' className='nav__link'>
             <BiUserPin />
-          </Link>
+          </NavLink>
         </li>
         <li className='nav__item'>
-          <button to='/darkmode' className='nav__link'>
+          <button className='nav__link'>
             <BiMoon />
           </button>
         </li>
         <li className='nav__item'>
-          <Link to='/contacts' className='nav__link'>
+          <NavLink to='/setting' className='nav__link'>
             <BiCog />
-          </Link>
+          </NavLink>
         </li>
       </ul>
       <div className='user'>
-        <img
-          className='user__avatar'
-          alt=''
+        <Avatar
+          size={36}
           src={`https://ui-avatars.com/api/?background=random&color=fff&name=DP`}
-        ></img>
+        />
       </div>
     </aside>
   );
