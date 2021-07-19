2ee06b44adc98de54a38e085b3520c949b4c11f5
Phong Duy Bui <buiduyphong921@gmail.com>
2021-07-19 23:07:49 +0700

    Update userSlice

---
+++ src/redux/slices/userSlice.js
@@ -0,0 +1,62 @@
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import { auth } from '../../firebase/config';
+import { addDocument, generateKeywords } from '../../firebase/services';
+
+const initialState = { userInfo: null };
+
+export const createUser = createAsyncThunk(
+  'user/createUser',
+  async (params) => {
+    await auth.createUserWithEmailAndPassword(params.email, params.password);
+    await auth.currentUser.updateProfile({
+      displayName: params.name,
+      photoURL: `https://ui-avatars.com/api/?background=random&color=fff&name=${params.name}`,
+    });
+
+    const { uid, displayName, photoURL, email, providerId } = auth.currentUser;
+
+    await addDocument('users', {
+      uid,
+      displayName,
+      photoURL,
+      email,
+      providerId,
+      keywords: generateKeywords(displayName.toLowerCase()),
+    });
+
+    return {
+      uid,
+      displayName,
+      photoURL,
+      email,
+    };
+  }
+);
+
+export const userSlice = createSlice({
+  name: 'user',
+  initialState,
+  reducers: {
+    signInAction: {
+      reducer: (state, action) => {
+        state.userInfo = action.payload;
+      },
+      prepare: (user) => {
+        return { payload: user };
+      },
+    },
+    signOutAction: () => {
+      return initialState;
+    },
+  },
+  extraReducers: (builder) => {
+    builder.addCase(createUser.fulfilled, (state, action) => {
+      state.userInfo = action.payload;
+    });
+  },
+});
+
+// Action creators are generated for each case reducer function
+export const { signInAction, signOutAction } = userSlice.actions;
+
+export default userSlice.reducer;
