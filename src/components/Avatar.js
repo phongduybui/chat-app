db92782a58bad6e335b93c1996d3507d7c6e4b77
Phong Duy Bui <buiduyphong921@gmail.com>
2021-07-20 23:46:49 +0700

    Add AvatarGroup component

--- src/components/Avatar.js
+++ src/components/Avatar.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import clsx from 'clsx';
 
-const Avatar = ({ src, size, hasBorder, hasState }) => {
+const Avatar = ({ src, size, hasBorder, userState, title }) => {
   return (
     <div
       className={clsx(
         'Avatar__wrapper',
         hasBorder && 'Avatar__has-border',
-        hasState && 'Avatar__has-state'
+        userState && `Avatar__has-state Avatar__has-state--${userState}`
       )}
       style={{
         width: size,
         height: size,
       }}
+      title={title}
     >
       <img
         src={src}
---
+++ src/components/AvatarGroup.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import clsx from 'clsx';
+
+const AvatarGroup = ({ children, max = 2, className }) => {
+  const childrenArray = React.Children.toArray(children);
+  const count = children.length;
+
+  const extraAvatars = count > max ? count - max + 1 : 0;
+
+  console.log({ count, max, extraAvatars });
+  return (
+    <div className={clsx('AvatarGroup', className)}>
+      {childrenArray.slice(0, count - extraAvatars + 1).map((child, i) => {
+        console.log(i);
+        return React.cloneElement(child, {
+          className: clsx('Avatar-child'),
+        });
+      })}
+      {extraAvatars && (
+        <div className='extra-avatar'>
+          <span>+{extraAvatars - 1}</span>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default AvatarGroup;
