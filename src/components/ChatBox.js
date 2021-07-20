4348bbb0d757cf9fc0d085a1a1e640ecea9cebfc
Phong Duy Bui <buiduyphong921@gmail.com>
2021-07-20 23:47:03 +0700

    Done ChatBox

--- src/components/ChatBox.js
+++ src/components/ChatBox.js
@@ -1,38 +1,69 @@
-import React, { useState } from 'react';
-import clsx from 'clsx';
+import React, { useState, useRef, useEffect } from 'react';
 import ChatInput from './ChatInput';
-import Avatar from './Avatar';
 import Message from './Message';
+import { useSelector } from 'react-redux';
+import { AiOutlineUsergroupAdd } from 'react-icons/ai';
+import { useRouteMatch } from 'react-router';
+import useFirestore from '../hooks/useFirestore';
+import InviteUserModal from './Modal/InviteUserModal';
 
 const ChatBox = () => {
-  const [activeTab, setActiveTab] = useState('message');
+  const match = useRouteMatch();
+  const [openInviteModal, setOpenInviteModal] = useState(false);
+  const { roomList } = useSelector((state) => state.rooms);
+  const { userInfo } = useSelector((state) => state.user);
+  const selectedRoom = roomList.find((r) => r.id === match.params.roomId);
+
+  const ref = useRef(null);
+
+  const messagesCondition = React.useMemo(
+    () => ({
+      fieldName: 'roomId',
+      operator: '==',
+      compareValue: match.params.roomId,
+    }),
+    [match.params.roomId]
+  );
+
+  const messages = useFirestore('messages', messagesCondition);
+
+  useEffect(() => {
+    if (messages && ref?.current) {
+      ref.current.scrollTo({
+        behavior: 'smooth',
+        top: ref.current.scrollHeight,
+      });
+    }
+  }, [messages]);
 
   return (
     <div className='ChatBox'>
       <div className='ChatBox__title'>
-        <h2 className='h1'>Group Chat</h2>
-        <div className='tabs'>
-          <button
-            className={clsx(activeTab === 'message' && 'active')}
-            onClick={() => setActiveTab('message')}
-          >
-            Message
-          </button>
-          <button
-            className={clsx(activeTab === 'participants' && 'active')}
-            onClick={() => setActiveTab('participants')}
-          >
-            Participants
-          </button>
-        </div>
+        <h2 className='h1'>{selectedRoom?.name}</h2>
+        <button
+          onClick={() => setOpenInviteModal(true)}
+          className='ChatBox__btn'
+        >
+          <AiOutlineUsergroupAdd />
+          <span>Invite</span>
+        </button>
       </div>
-      <div className='ChatBox__wrapper-content'>
-        <Message test='abcdtest' />
 
-        <Message />
-        <Message />
+      <InviteUserModal
+        open={openInviteModal}
+        onClose={() => setOpenInviteModal(false)}
+        currentMembers={selectedRoom?.members}
+        roomId={match.params.roomId}
+      />
+
+      <div className='ChatBox__wrapper-content'>
+        <div className='ChatBox__messages' ref={ref}>
+          {messages.map((message) => (
+            <Message key={message.id} {...message} />
+          ))}
+        </div>
+        <ChatInput userInfo={userInfo} roomId={match.params.roomId} />
       </div>
-      <ChatInput />
     </div>
   );
 };
